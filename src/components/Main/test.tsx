1cd75a06a8f0fadffb98d852f6f2670cb16fffeb
Bruno N. Campos <brunosopmac@gmail.com>
2021-09-29 18:45:50 -0300

    Boilerplate Finalizado

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,17 +1,17 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Main from '.'
 
 describe('<Main />', () => {
-  // it('should render a heading', () => {
-  //   const { container } = render(<Main />)
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
 
-  //   expect(
-  //     screen.getByRole('heading', { name: /react avançado/i })
-  //   ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
 
-  //   expect(container.firstChild).toMatchSnapshot()
-  // })
+    expect(container.firstChild).toMatchSnapshot()
+  })
 
   it('should render the colors correctly', () => {
     const { container } = render(<Main />)
